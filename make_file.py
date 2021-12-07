b3498a78456ffedbdf79408c0354019f1ead5114
Richard Alvero <richiealvero@gmail.com>
2021-12-07 17:31:52 -0500

    Update make_file.py

--- make_file.py
+++ make_file.py
@@ -3,8 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'https://www.imdb.com/search/title/?title_type=feature,tv_movie&release_date=1950-01-01,2021-12-31&languages=en&view=simple&count=250'
-#'https://www.imdb.com/search/title/?title_type=feature,tv_movie&release_date=1950-01-01,2021-12-31&languages=en&start=51&ref_=adv_nxt'
-#'https://www.imdb.com/search/title/?title_type=feature,tv_movie&release_date=1950-01-01,2021-12-31&languages=en&start=101&ref_=adv_nxt'
+# 'https://www.imdb.com/search/title/?title_type=feature,tv_movie&release_date=1950-01-01,2021-12-31&languages=en&view=simple&count=250&start=251&ref_=adv_nxt'
+# 'https://www.imdb.com/search/title/?title_type=feature,tv_movie&release_date=1950-01-01,2021-12-31&languages=en&view=simple&count=250&start=251&ref_=adv_nxt'
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 # print(soup)
@@ -14,6 +14,9 @@ link = []
 page_data = soup.findAll('div', attrs={'class': 'lister-item mode-simple'})
 
 for blocks in page_data:
-    link = blocks.div.a.text
+    movieData = blocks.div.a
+    movieID = movieData.get('href')
+    movieID = movieData[9:16]
+    link.append(movieID)
 
 print(link)
