78ad65775242a960a45c78898ae8e985ad12eb6c
Richard Alvero <richiealvero@gmail.com>
2021-12-07 17:08:56 -0500

    Update make_graph.py
    More soup

--- make_graph.py
+++ make_graph.py
@@ -2,13 +2,21 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.imdb.com/search/title/?title_type=feature,tv_movie&release_date=1950-01-01,2021-12-31&languages=en'
+url = 'https://www.imdb.com/search/title/?title_type=feature,tv_movie&release_date=1950-01-01,2021-12-31&languages=en&view=simple&count=250'
+#'https://www.imdb.com/search/title/?title_type=feature,tv_movie&release_date=1950-01-01,2021-12-31&languages=en&start=51&ref_=adv_nxt'
+#'https://www.imdb.com/search/title/?title_type=feature,tv_movie&release_date=1950-01-01,2021-12-31&languages=en&start=101&ref_=adv_nxt'
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
-print(soup)
+# print(soup)
 
+link = []
 
+page_data = soup.findAll('div', attrs={'class': 'lister-item mode-simple'})
 
+for blocks in page_data:
+    link = blocks.div.a.text
+
+print(link)
 
 
 
