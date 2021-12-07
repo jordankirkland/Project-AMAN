e5517e7d766ed829199ffb633bf0dc438912f859
Richard Alvero <richiealvero@gmail.com>
2021-12-07 16:42:09 -0500

    Update make_graph.py
    Commented out imdbpy and switched to testing beautiful soup, though right now imdb is down and all it's scraping is the error site

--- make_graph.py
+++ make_graph.py
@@ -1,13 +1,31 @@
-from imdb import IMDb 
-ia = IMDb()
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.imdb.com/search/title/?title_type=feature,tv_movie&release_date=1950-01-01,2021-12-31&languages=en'
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+print(soup)
 
 
-# Function get_movies() using beautiful soup and the url to scrape 
-# movie names from our search and store them in a .txt file
 
 
-# Read in each movie and use imdbpy to access the actor lists?
-matrix = ia.get_movie('1160419')
-matrix.infoset2keys
-for item in matrix['cast']:
-    print(item)
+
+
+
+
+#Imdbpy stuff I'm ignoring for now
+
+# from imdb import IMDb 
+# ia = IMDb()
+
+
+# # Function get_movies() using beautiful soup and the url to scrape 
+# # movie names from our search and store them in a .txt file
+
+
+# # Read in each movie and use imdbpy to access the actor lists?
+# matrix = ia.get_movie('0133093')
+# matrix.infoset2keys
+# for item in matrix['cast']:
+#     print(item)
