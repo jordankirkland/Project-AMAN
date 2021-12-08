738dc9bb10f095cd924fbb964790873173339718
Richard Alvero <richiealvero@gmail.com>
2021-12-08 14:03:24 -0500

    Actor and make graph updates

--- Actor.py
+++ Actor.py
@@ -2,6 +2,8 @@ class Actor:
     # First actor in list is self in case a request asks for distance to self
     def __init__(self, name, actor, movie):
         self.name = name
-        self.actors = [name]
+        
+        # Map of actors costarring with. Value is a shared movie
+        self.actors = {}
         self.movies = [movie]
 
