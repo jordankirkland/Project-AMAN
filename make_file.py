4153f333cf04daa8abaacbd3a0e5c8415b74f50e
jkirkland722 <jordan.kirkland@ufl.edu>
2021-12-07 22:37:52 -0500

    Fixed ID Reader

--- make_file.py
+++ make_file.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import imdb
+ia = imdb.IMDb()
 
 url = 'https://www.imdb.com/search/title/?title_type=feature,tv_movie&release_date=1950-01-01,2021-12-31&languages=en&view=simple&count=250'
 # 'https://www.imdb.com/search/title/?title_type=feature,tv_movie&release_date=1950-01-01,2021-12-31&languages=en&view=simple&count=250&start=251&ref_=adv_nxt'
@@ -9,14 +11,21 @@ response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 # print(soup)
 
-link = []
+movieIDs = []
 
 page_data = soup.findAll('div', attrs={'class': 'lister-item mode-simple'})
 
 for blocks in page_data:
     movieData = blocks.div.a
     movieID = movieData.get('href')
-    movieID = movieData[9:16]
-    link.append(movieID)
+    #since the codes are different lengths, just take all until the end of href block which goes until /
+    movieID = movieID[9:18]
+    #remove '/'
+    movieID = movieID.replace('/', '')
+    movieIDs.append(movieID)
 
-print(link)
+
+movies = []
+for ID in movieIDs:
+    movie = ia.get_movie(ID)
+    print(movie)
