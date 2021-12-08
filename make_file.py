c18af8c759cf8f2ced26a00ad74e9cf28caeb3e6
jkirkland722 <jordan.kirkland@ufl.edu>
2021-12-08 14:02:24 -0500

    Generates IDs for ~80 pages

--- make_file.py
+++ make_file.py
@@ -2,29 +2,38 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import imdb
-#import omdb
 ia = imdb.IMDb()
 
-url = 'https://www.imdb.com/search/title/?title_type=feature,tv_movie,tv_series,tv_miniseries&release_date=,2021-11-01&plot=marvel&view=simple&count=250'
-# 'https://www.imdb.com/search/title/?title_type=feature,tv_movie&release_date=1950-01-01,2021-12-31&languages=en&view=simple&count=250&start=251&ref_=adv_nxt'
-# 'https://www.imdb.com/search/title/?title_type=feature,tv_movie&release_date=1950-01-01,2021-12-31&languages=en&view=simple&count=250&start=501&ref_=adv_nxt'
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-# print(soup)
+# url = 'https://www.imdb.com/search/title/?title_type=feature,tv_movie,tv_series,tv_miniseries&release_date=,2021-11-01&plot=marvel&view=simple&count=250'
+url = 'https://www.imdb.com/search/title/?title_type=feature,tv_movie&release_date=1950-01-01,2021-11-01&languages=en&view=simple&count=250'
 
 movieIDs = []
 actors = {}
 
-page_data = soup.findAll('div', attrs={'class': 'lister-item mode-simple'})
 
-for blocks in page_data:
-    movieData = blocks.div.a
-    movieID = movieData.get('href')
-    #since the codes are different lengths, just take all until the end of href block which goes until /
-    movieID = movieID[9:18]
-    #remove '/'
-    movieID = movieID.replace('/', '')
-    movieIDs.append(movieID)
+#function for generating the ids on a page
+def generateIDs(pageUrl):
+    response = requests.get(pageUrl)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    page_data = soup.findAll('div', attrs={'class': 'lister-item mode-simple'})
+
+    for blocks in page_data:
+        movieData = blocks.div.a
+        movieID = movieData.get('href')
+        #since the codes are different lengths, just take all until the end of href block which goes until /
+        movieID = movieID[9:18]
+        #remove '/'
+        movieID = movieID.replace('/', '')
+        movieIDs.append(movieID)
+
+
+#generates ids for 2000 movies
+generateIDs(url)
+pageStart = 251
+while (pageStart < 20000):
+    url = 'https://www.imdb.com/search/title/?title_type=feature,tv_movie&release_date=1950-01-01,2021-11-01&languages=en&view=simple&count=250&start=' + str(pageStart) + '&ref_=adv_nxt'
+    generateIDs(url)
+    pageStart += 250
 
 
 uniqueActors = set()
@@ -56,9 +65,9 @@ for ID in movieIDs:
 output_data = actors.items()
 output_list = list(output_data)
 
-# Should make a basic column sheet, still need to think about how to handle duplicate actors though, maybe through 
+# Should make a basic column sheet, still need to think about how to handle duplicate actors though, maybe through
 # 2d array of movies? Though that may require a map to reaccess the actor with relatively fast time.
 sheet = pd.DataFrame(output_list)
 
-sheet.to_csv('vertices.csv', index = False)
-#Will is practicing commit
+sheet.to_csv('vertices.csv', index=False)
+# Will is practicing commit
