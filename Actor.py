70e28745792c56cfc780c629dd24a5a416b000db
Richard Alvero <richiealvero@gmail.com>
2021-12-08 15:30:10 -0500

    Updating make_graph to read from file

--- Actor.py
+++ Actor.py
@@ -1,6 +1,6 @@
 class Actor:
     # First actor in list is self in case a request asks for distance to self
-    def __init__(self, name, actor, movie):
+    def __init__(self, name, movie):
         self.name = name
         
         # Map of actors costarring with. Value is a shared movie
--- make_graph.py
+++ make_graph.py
@@ -1,4 +1,5 @@
 import csv
+from Actor import Actor
 from imdb import IMDb 
 
 # Map of actor name strings to Actor objects
@@ -9,10 +10,34 @@ currentRow = []
 
 with open('vertices.csv', 'r') as file:
     reader = csv.reader(file)
+    
+    # For each row in the file
     for row in reader:
-        currentRow = row
 
-# Read in each movie and use imdbpy to access the actor lists?
-matrix = ia.get_movie('0133093')
-for item in matrix['cast']:
-    print(item)
+        # Temp variables for movie
+        currentRow = row
+        movie = row[0]
+
+        # For each actor in the row
+        for actor in row[1:]:
+            if actor == '':
+                break
+            # If the actor exists in the graph already, append the movie
+            # and actor to the list and map respectively
+            if actor.lower() in graph:
+                graph[actor.lower()].movies.append(movie)
+                for vertice in row[1:]:
+                    if actor == '':
+                        break
+                    graph[actor.lower()].actors[vertice.lower()] = movie
+            else:
+                graph[actor.lower()] = Actor(actor, movie)
+                for vertice in row[1:]:
+                    if actor == '':
+                        break
+                    graph[actor.lower()].actors[vertice.lower()] = movie
+print("hi")
+# # Read in each movie and use imdbpy to access the actor lists?
+# matrix = ia.get_movie('0133093')
+# for item in matrix['cast']:
+#     print(item)
