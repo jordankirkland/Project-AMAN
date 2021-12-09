28263634f2ba012fd0a1066d95014782f86cdc0b
jkirkland722 <jordan.kirkland@ufl.edu>
2021-12-08 19:08:46 -0500

    Fixed csv and added some text to main

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import make_graph
 
+print("Initiating...")
+graph = make_graph.Graph()
+print("Welcome to Project-AMAN")
+print("a. Shortest Chain of Connection with Dijkstra's")
+print("b. Shortest Chain of Connection with Breadth-First Search")
 
 
